f96df2d174070aa603f254861c8b526e150f5667
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-23 10:19:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 let weather = {
-    "apiKey": f6829966d703e117af245e49ec847b4a
-        .fetchWeather: function(Town) {
-            fetch(
-                    "https://api.openweathermap.org/data/2.5/weather?lat={18}&lon={18}&appid={f6829966d703e117af245e49ec847b4a"
-                ).then((response) => response.json())
-                .then((data) => this.displayweather(data));
-        }
-    displayweather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather: [0];
-        const { temperature, humidity } = data.main;
-        const { speed } = data.wind;
-        console.log(name, icon, description, temperature, humidity, speed)
-        document.querySelector("Town").innerText = "weather in" + name
-        document.querySelector(".icon").src = "https://api.openweathermap.org/data/2.5/weather? + icon + lat={lat}lon={lon}&appid={API key"
-
+    "apiKey": "f6829966d703e117af245e49ec847b4a"
+    fetchweather: function() {
+        fetch(
+                https: //api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={cc627cb806829dd4c73c00f273ea7725} 
+            ).then((response) => response.json())
+            .then((data) => console.log(data));
     }
 }
